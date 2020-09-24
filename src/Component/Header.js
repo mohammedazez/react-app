5c4c0870f29c372c0f87a827c2e585288ae37a82
azezmohammed <muhamadaziz047@gmail.com>
2020-09-25 00:57:31 +0700

    Make it styling

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 // Import hooks redux
 import { useSelector } from "react-redux";
@@ -9,13 +10,27 @@ function Header() {
   const total = useSelector((state) => state.totalBelanja);
   return (
     <div>
-      <img
-        src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG42.png"
-        alt="cart"
-        className="picture"
-      />
-      {selector}
-      <p>{total}</p>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Beli Bakso</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <img
+              src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG42.png"
+              alt="cart"
+              className="picture"
+            />
+            <Nav.Link href="#deets">Jumlah mangkok : {selector}</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Total Harga : {total}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "./Cart.css";
+// import bakso from "../Assets/bakso.webp";
 // Import action
 import { handlePlus, handleMinus } from "../Redux/Action/cart.action";
 // Import hooks redux
@@ -8,34 +11,49 @@ function Cart() {
     {
       name: "Bakso",
       price: 25000,
+      img: require("../Assets/bakso.webp"),
     },
   ];
   const selector = useSelector((state) => state.cart);
   const total = useSelector((state) => state.totalBelanja);
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className="container">
       <div>
         {product.map((item) => (
           <div>
-            <p>{item.name}</p>
-            <p>{item.price}</p>
-            <h1>Ini halaman Cart</h1>
-            <button
-              onClick={() => {
-                dispatch(handlePlus(item));
-              }}
-            >
-              +
-            </button>
-            <h1>{selector}</h1>
-            <button
-              onClick={() => {
-                dispatch(handleMinus(item));
-              }}
-            >
-              -
-            </button>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://indonesia.tripcanvas.co/id/wp-content/uploads/sites/2/2018/01/4-4-optimized-via-renna_susan.gif"
+              />
+              {/* <img src="{item.img}" alt="bakso" /> */}
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <p> Harga : {item.price}</p>
+                <p> Total: {total}</p>
+                <p>Kuantitas: {selector}</p>
+                <Button
+                  className="tombol"
+                  variant="primary"
+                  onClick={() => {
+                    dispatch(handleMinus(item));
+                  }}
+                >
+                  Kurangi
+                </Button>
+                <Button
+                  className="tombol"
+                  variant="primary"
+                  onClick={() => {
+                    dispatch(handlePlus(item));
+                  }}
+                >
+                  Tambah
+                </Button>
+              </Card.Body>
+            </Card>
+
             {/* <button
               onClick={() => {
                 dispatch(harga(item));
@@ -43,7 +61,6 @@ function Cart() {
             >
               Beli
             </button> */}
-            <p>{total}</p>
           </div>
         ))}
       </div>
